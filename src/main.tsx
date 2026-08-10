9d4f8fb73a569361d84cb111a12c76c0aa9dda5d
MUSTAFA <shayadkhizarhogaa@gmail.com>
2026-08-10 22:24:39 +0500

    fix: update google client ID

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = "553701188220-1cbiu2rgafij38mluj3tqi1fdlf6sb2c.apps.googleusercontent.com"
+const GOOGLE_CLIENT_ID = "553701188220-1cbiu2rgaftj38mluj3tqi1fdlf6sb2c.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
